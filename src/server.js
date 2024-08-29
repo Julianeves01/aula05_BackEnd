9f4a1bdde167bfbcf1af6e10135777d23e28aabb
Julianeves01 <julianeves.tds1.senai@gmail.com>
2024-08-29 15:51:43 -0300

    feat: add some requests

--- src/server.js
+++ src/server.js
@@ -6,11 +6,59 @@ config()
 const port = process.env.PORT || 3000
 
 const app = express()
-
 app.use(express.json())
+
+const guloseimas = [
+    {
+        id: 1,
+        nome:"Trufa",
+        preco: 4.50
+    },
+    {
+        id: 2,
+        nome: "Sorvete",
+        preco: 8.60
+    },
+    {
+        id: 3,
+        nome: "Petit gateau",
+        preco: 12.00
+    }
+]
+
+const filmesMarcantes = [
+    {
+        id: 1001,
+        titulo: "Milagre na cela 7",
+        genero: "Drama",
+        emCartaz: false
+    },
+    {
+        id: 1002,
+        titulo: "Divertidamente2",
+        genero: "Animação",
+        emCartaz: false
+    },
+    {
+        id: 1003,
+        titulo: "Meninas malvadas",
+        genero: "Comedia, romance, adolescente, drama",
+        emCartaz: false
+    }
+]
+
+app.get("/filmes", (req, res) => {
+    return res.status(200). send(filmesMarcantes);
+})
+
+
 
 app.get("/", (req, res) => { //request, response
     return res.status(200). send({ message: "Hello, world!"})
+})
+
+app.get("/doces", (req, res) => {
+    return res.status(200). send(guloseimas)
 })
 
 app.listen(port, () => {
